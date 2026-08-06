cebe9228da825fbd1f39d94de62053695e3a1530
Shaheer ul Hassan <samjanjua6@gmail.com>
2026-08-06 14:20:57 +0500

    fix: Update CORS to allow live domain

--- app/main.py
+++ app/main.py
@@ -31,7 +31,11 @@ app = FastAPI(
 # CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"] if settings.APP_ENV == "development" else [],
+    allow_origins=[
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+        "https://haji-cafe.mychatbot.codes",
+    ],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
